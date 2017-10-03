f8f33018ef3fd55c2d207d44a8e55256eab53594
Adrian Humphreys <adrian.humphreys@enlighten.co.nz>
2017-10-04 09:04:52 +1300

    scaled in half

--- themes/bae/src/webpack.config.js
+++ themes/bae/src/webpack.config.js
@@ -4,56 +4,59 @@ var webpack = require("webpack");
 
 module.exports = {
 	entry: {
-		app: './js/main.js'
+		app: "./js/main.js"
 	},
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-		      	exclude: /node_modules/,
-		      	use: {
-		        	loader: 'babel-loader',
-		        	options: {
-		          		presets: ['env']
-		          		// plugins: [require('babel-plugin-transform-object-rest-spread')]
-		        	}
-		      	}
-		    },
-		    {
-		    	test: /\.css$/,
-		    	use: ExtractTextPlugin.extract({
-		    			fallback: { loader: 'style-loader' },
-		    			use: [{
-		    				loader: 'css-loader',
-		    				options: {
-									importLoaders: 1,
-									url: false
-		    				}
-		    			}, {
-		    				loader: 'postcss-loader'
-		    			}]
-		    		})
-		    },
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+					options: {
+						presets: ["env"]
+						// plugins: [require('babel-plugin-transform-object-rest-spread')]
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ExtractTextPlugin.extract({
+					fallback: { loader: "style-loader" },
+					use: [
+						{
+							loader: "css-loader",
+							options: {
+								importLoaders: 1,
+								url: false
+							}
+						},
+						{
+							loader: "postcss-loader"
+						}
+					]
+				})
+			}
 		]
 	},
 
 	output: {
-    path: path.join(__dirname, "./../static/dist"),
-		filename: '[name].bundle.js',
+		path: path.join(__dirname, "./../static/dist"),
+		filename: "[name].bundle.js"
 	},
 
 	resolve: {
-		modules: [path.resolve(__dirname, 'src'), 'node_modules']
+		modules: [path.resolve(__dirname, "src"), "node_modules"]
 	},
 
 	plugins: [
 		new ExtractTextPlugin("main.css"),
 		new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    }),
+			$: "jquery",
+			jQuery: "jquery"
+		})
 	],
 	watchOptions: {
 		watch: true
 	}
-}
+};
